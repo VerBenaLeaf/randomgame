e75454b1da84f3db16d1536426f6e2501604ab91
Dakyeong Lee <dlekrud0503@gmail.com>
2016-03-31 20:59:12 +0900

    #3 add button and onClickListner

--- app/src/main/java/kr/hs/emirim/good005003/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/good005003/monchicken/MainActivity.java
@@ -1,12 +1,17 @@
 package kr.hs.emirim.good005003.monchicken;
 
+import android.content.DialogInterface;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListner {
 
     EditText mName;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mName = (EditText) findViewById(R.id.name);
+        mButton = (Button) findViewById(R.id.btn_show_me_the_chicken);
+        mButton.setOnclickListner(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         mName.setText("");
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "배고파요!", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/kr/hs/emirim/good005003/monchicken/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/good005003/monchicken/ResultActivity.java
@@ -6,24 +6,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    TextView mResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mResult = (TextView) findViewById(R.id.result);
+        mResult.setText("결과");
+
+        };
     }
 
 }
