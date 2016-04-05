c2796761ad2f8af2a3a44e2e7e4d61311c7593b5
Dakyeong Lee <dlekrud0503@gmail.com>
2016-04-05 19:04:17 +0900

    #2 add intent code

--- app/src/main/java/kr/hs/emirim/good005003/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/good005003/monchicken/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.good005003.monchicken;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -7,19 +8,24 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.view.View;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     EditText mName;
     Button mButton;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mName = (EditText) findViewById(R.id.name);
+        mName = (EditText) findViewById(R.id.name);5
         mButton = (Button) findViewById(R.id.btn_show_me_the_chicken);
         mButton.setOnClickListener(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
     }
 
     @Override
@@ -31,5 +37,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         Toast.makeText(this, "배고파요!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
     }
 }
