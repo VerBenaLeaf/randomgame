69749beac3d9b7e7edfe332ed13849a0234bf0d0
Dakyeong Lee <dlekrud0503@gmail.com>
2016-04-05 20:32:17 +0900

    #10 handle user input(name)

--- app/src/main/java/kr/hs/emirim/good005003/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/good005003/monchicken/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mName = (EditText) findViewById(R.id.name);5
+        mName = (EditText) findViewById(R.id.name);
         mButton = (Button) findViewById(R.id.btn_show_me_the_chicken);
         mButton.setOnClickListener(this);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
@@ -31,13 +31,32 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
-        mName.setText("");
+        mName.setText(null);
     }
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "배고파요!", Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(this, ResultActivity.class);
-        startActivity(intent);
+
+        String name = mName.getText().toString();
+
+//        if (name == null) {
+//            Toast.makeText(this, "이름을 입력해 주세요!", Toast.LENGTH_LONG).show();
+//        } else {
+//
+//            Toast.makeText(this, name + "씨, 배고파요!", Toast.LENGTH_LONG).show();
+//            Intent intent = new Intent(this, ResultActivity.class);
+//            startActivity(intent);
+//        }
+
+        try {
+            Toast.makeText(this, name + "씨, 배고파요!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, ResultActivity.class);
+            startActivity(intent);
+
+        } catch(NullPointerException e) {
+            Toast.makeText(this, "이름을 입력해 주세요!", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "뭔지 모르지만 잘 안되네요!", Toast.LENGTH_LONG).show();
+        }
     }
 }
