7fa63e965f5b67ef12dc4dd277464542e6a28d6d
Dakyeong Lee <dlekrud0503@gmail.com>
2016-04-12 19:27:51 +0900

    #7 Activity page change

---
+++ app/src/main/java/kr/hs/emirim/good005003/monchicken/SplashActivity.java
@@ -0,0 +1,30 @@
+package kr.hs.emirim.good005003.monchicken;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by appcreator33 on 2016. 4. 12..
+ */
+public class SplashActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable () {
+
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, R.anim.zoom_exit);
+                finish();
+            }
+        }, 3000);
+    }
+}
